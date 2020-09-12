9bc629d6bf20cec862f0c46a4349190165c1fb69
Harsh Raj <me@harshraj.xyz>
2020-09-12 16:42:36 +0530

    Notes on JS

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-var a=10;
-if(a%2==0){
-    console.log("Even");
-}
-else{
-    console.log("odd");
+// var a=10;
+// if(a%2==0){
+//     console.log("Even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+for(var i=1;i>0;i++){
+    if(i==5){
+        continue;
+    }
+    if(i==12){
+        break;
+    }
+    console.log(i);
 }
