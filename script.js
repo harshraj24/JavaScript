6cc0bbdf5220d036e6e64221c1b31ba7f0686053
Harsh Raj <me@harshraj.xyz>
2020-09-12 19:26:19 +0530

    Sorting Alogo

--- script.js
+++ script.js
@@ -6,12 +6,15 @@
 //     console.log("odd");
 // }
 
-for(var i=1;i>0;i++){
-    if(i==5){
-        continue;
-    }
-    if(i==12){
-        break;
-    }
-    console.log(i);
-}
+// for(var i=1;i>0;i++){
+//     if(i==5){
+//         continue;
+//     }
+//     if(i==12){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+var arr=["Harsh","Abhi","Bharati"]
+console.log(arr.sort())
