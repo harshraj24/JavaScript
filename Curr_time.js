f33751eff3817e73b02bcee928143840fb4218a3
Harsh Raj <me@harshraj.xyz>
2020-09-17 23:48:50 +0530

    Added date function

--- Curr_time.js
+++ Curr_time.js
@@ -1,11 +1,14 @@
 let a;
 let curr_time;
 let clr;
+let dt;
 
 clr= setInterval(() => {
     a = new Date();
-    curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+    dt=a.toLocaleDateString();
+    curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds() + " on " + dt;
     document.getElementById('time').innerHTML=curr_time;
+    
 
 }, 1000);
 
@@ -16,7 +19,7 @@ document.getElementById('stop').addEventListener('click',function(){
 document.getElementById('start').addEventListener('click',function(){
     clr= setInterval(() => {
         a = new Date();
-        curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+        curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds() + " on " + dt;
         document.getElementById('time').innerHTML=curr_time;
     
     }, 1000);
