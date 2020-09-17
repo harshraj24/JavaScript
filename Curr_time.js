4bda93b385cd1b9b50cf0a4a891bdb77ccd51405
Harsh Raj <me@harshraj.xyz>
2020-09-17 23:45:07 +0530

    Added a stop Button

--- Curr_time.js
+++ Curr_time.js
@@ -12,3 +12,12 @@ clr= setInterval(() => {
 document.getElementById('stop').addEventListener('click',function(){
     clearInterval(clr)
 })
+
+document.getElementById('start').addEventListener('click',function(){
+    clr= setInterval(() => {
+        a = new Date();
+        curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+        document.getElementById('time').innerHTML=curr_time;
+    
+    }, 1000);
+})
