27beedc50b1c26fc1db366047faa2acddb68a7cf
Harsh Raj <me@harshraj.xyz>
2020-09-18 23:00:52 +0530

    Minor

--- Curr_time.js
+++ Curr_time.js
@@ -6,7 +6,7 @@ const options = {weekdays:'long',year:'numeric',month:'long',day:'numeric'}
 clr= setInterval(() => {
     a = new Date();
     dt=a.toLocaleDateString(undefined,options);
-    curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds() + " <br> on <br>" + dt;
+    curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds() + " <br> on " + dt;
     document.getElementById('time').innerHTML=curr_time;
     
 
@@ -19,7 +19,7 @@ document.getElementById('stop').addEventListener('click',function(){
 document.getElementById('start').addEventListener('click',function(){
     clr= setInterval(() => {
         a = new Date();
-        curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds() + "<br> on <br>" + dt;
+        curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds() + "<br> on " + dt;
         document.getElementById('time').innerHTML=curr_time;
     
     }, 1000);
