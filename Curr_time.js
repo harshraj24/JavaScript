bc24a711904f519b11fa101c692382f0257a0920
Harsh Raj <me@harshraj.xyz>
2020-09-18 20:52:07 +0530

    minor changes

--- Curr_time.js
+++ Curr_time.js
@@ -2,11 +2,11 @@ let a;
 let curr_time;
 let clr;
 let dt;
-
+const options = {weekdays:'long',year:'numeric',month:'long',day:'numeric'}
 clr= setInterval(() => {
     a = new Date();
-    dt=a.toLocaleDateString();
-    curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds() + " on " + dt;
+    dt=a.toLocaleDateString(undefined,options);
+    curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds() + " <br> on <br>" + dt;
     document.getElementById('time').innerHTML=curr_time;
     
 
@@ -19,7 +19,7 @@ document.getElementById('stop').addEventListener('click',function(){
 document.getElementById('start').addEventListener('click',function(){
     clr= setInterval(() => {
         a = new Date();
-        curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds() + " on " + dt;
+        curr_time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds() + "<br> on <br>" + dt;
         document.getElementById('time').innerHTML=curr_time;
     
     }, 1000);
